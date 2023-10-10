73e06de3df3c5c75fae3af67acc21f08e14d7003
Maneesh Cheema <ayechico21@gmail.com>
2023-10-10 23:16:52 +0530

    added button sounds

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,19 +8,41 @@ import { useToggle } from "../../hooks";
 import SettingsModal from "../Modals/SettingsModal";
 import InfoModal from "../Modals/InfoModal";
 import { AppContext } from "../Providers/AppProvider";
-
+import useSound from "use-sound";
+import { abort, singleTick } from "../../sounds";
 function Header() {
   const { setGameStatus } = React.useContext(AppContext);
   const [isInfo, toggleInfo] =
     useToggle(false); /**vy default, Modal is not shown */
   const [isSettings, toggleSettings] = useToggle(false);
 
+  const [singleTickSound] = useSound(singleTick);
+  const [abortSound] = useSound(abort, { volume: 1 });
+
   return (
     <Wrapper>
-      <IconButton icon={<Info />} onClick={toggleInfo} />
+      <IconButton
+        icon={<Info />}
+        onClick={() => {
+          singleTickSound();
+          toggleInfo();
+        }}
+      />
       <Heading>Wordle</Heading>
-      <IconButton icon={<Settings />} onClick={toggleSettings} />
-      <IconButton icon={<RefreshCw />} onClick={() => setGameStatus("end")} />
+      <IconButton
+        icon={<Settings />}
+        onClick={() => {
+          singleTickSound();
+          toggleSettings();
+        }}
+      />
+      <IconButton
+        icon={<RefreshCw />}
+        onClick={() => {
+          abortSound();
+          setGameStatus("end");
+        }}
+      />
 
       {isInfo && (
         <Modal handleDismiss={toggleInfo}>
