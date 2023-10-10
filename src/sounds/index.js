c7e5932a931f332429e73480ec8500b7993f3a9c
Maneesh Cheema <ayechico21@gmail.com>
2023-10-10 23:16:35 +0530

    added sounds

---
+++ src/sounds/index.js
@@ -0,0 +1,23 @@
+import singleTick from "./single-tick.mp3";
+import abort from "./abort.mp3";
+import activate from "./activate.mp3";
+import lost from "./lost.mp3";
+import popDown from "./pop-down.mp3";
+import popUp from "./pop-up-on.mp3";
+import shuffleCards from "./shuffle-cards.mp3";
+import typing from "./typing.mp3";
+import warning from "./warning.mp3";
+import won from "./won.mp3";
+
+export {
+  singleTick,
+  abort,
+  activate,
+  lost,
+  popDown,
+  popUp,
+  shuffleCards,
+  typing,
+  warning,
+  won,
+};
